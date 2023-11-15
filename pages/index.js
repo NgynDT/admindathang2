d1ae068ce623679e84a3a11505c255878f624428
NgynDT <NgynDT>
2023-11-15 17:32:55 +0000

    Pushed from Locofy Builder

---
+++ pages/index.js
@@ -0,0 +1,127 @@
+import styles from "./index.module.css";
+
+const AdminPhiuCHng = () => {
+  return (
+    <div className={styles.adminPhiuCHng}>
+      <img
+        className={styles.adminPhiuCHngChild}
+        alt=""
+        src="/rectangle-57.svg"
+      />
+      <div className={styles.adminPhiuCHngItem} />
+      <div className={styles.trangQungTr}>Trang quảng trị</div>
+      <img
+        className={styles.akarIconsstatisticUp}
+        alt=""
+        src="/akariconsstatisticup.svg"
+      />
+      <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+      <div className={styles.thngK}>Thống kê</div>
+      <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+      <div className={styles.qunLSn}>Quản lý sản phẩm</div>
+      <img className={styles.vaadinchartIcon} alt="" src="/vaadinchart.svg" />
+      <div className={styles.danhSchHa}>Danh sách hóa đơn</div>
+      <img className={styles.vectorIcon2} alt="" src="/vector2.svg" />
+      <img className={styles.vectorIcon3} alt="" src="/vector2.svg" />
+      <img className={styles.vectorIcon4} alt="" src="/vector2.svg" />
+      <div className={styles.thitLp}>Thiết lập</div>
+      <img
+        className={styles.iconEnvelopeClosed}
+        alt=""
+        src="/-icon-envelope-closed.svg"
+      />
+      <img className={styles.vectorIcon5} alt="" src="/vector2.svg" />
+      <img className={styles.vectorIcon6} alt="" src="/vector2.svg" />
+      <img className={styles.vectorIcon7} alt="" src="/vector2.svg" />
+      <img className={styles.iconBell} alt="" src="/-icon-bell.svg" />
+      <div className={styles.admin}>Admin</div>
+      <div className={styles.adminPhiuCHngInner} />
+      <div className={styles.adminPhiuContainer}>
+        <span>Admin</span>
+        <span className={styles.span}>{` `}</span>
+        <span className={styles.phiuTHng}>/ Phiếu đặt hàng</span>
+      </div>
+      <div className={styles.rectangleDiv} />
+      <div className={styles.phiuTHng1}>Phiếu đặt hàng</div>
+      <img className={styles.lineIcon} alt="" src="/line-6.svg" />
+      <div className={styles.lineDiv} />
+      <div className={styles.adminPhiuCHngChild1} />
+      <div className={styles.hin}>Hiện</div>
+      <img className={styles.frameIcon} alt="" src="/frame-33.svg" />
+      <div className={styles.mc}>mục</div>
+      <div className={styles.tmKim}>Tìm kiếm:</div>
+      <div className={styles.adminPhiuCHngChild2} />
+      <div className={styles.adminPhiuCHngChild3} />
+      <div className={styles.adminPhiuCHngChild4} />
+      <div className={styles.adminPhiuCHngChild5} />
+      <div className={styles.adminPhiuCHngChild6} />
+      <div className={styles.adminPhiuCHngChild7} />
+      <div className={styles.adminPhiuCHngChild8} />
+      <div className={styles.adminPhiuCHngChild9} />
+      <div className={styles.adminPhiuCHngChild10} />
+      <div className={styles.adminPhiuCHngChild11} />
+      <div className={styles.adminPhiuCHngChild12} />
+      <div className={styles.adminPhiuCHngChild13} />
+      <div className={styles.adminPhiuCHngChild14} />
+      <div className={styles.adminPhiuCHngChild15} />
+      <div className={styles.adminPhiuCHngChild16} />
+      <div className={styles.adminPhiuCHngChild17} />
+      <div className={styles.adminPhiuCHngChild18} />
+      <div className={styles.adminPhiuCHngChild19} />
+      <div className={styles.adminPhiuCHngChild20} />
+      <div className={styles.mKh}>Mã KH</div>
+      <div className={styles.tnKh}>Tên KH</div>
+      <div className={styles.ngyT}>Ngày đặt</div>
+      <div className={styles.snPhm}>Sản phẩm</div>
+      <div className={styles.tnhTrng}>Tình trạng</div>
+      <div className={styles.khach}>khach</div>
+      <div className={styles.ph}>Phú</div>
+      <div className={styles.thanhTon}>Đã thanh toán</div>
+      <div className={styles.chiTit}>
+        <p className={styles.blankLine}>Chi tiết</p>
+      </div>
+      <div className={styles.chiTit2}>Chi tiết</div>
+      <div className={styles.chiTit3}>Chi tiết</div>
+      <div className={styles.chiTit4}>Chi tiết</div>
+      <div className={styles.xa}>Xóa</div>
+      <div className={styles.xa1}>Xóa</div>
+      <div className={styles.thanhTon1}>Đã thanh toán</div>
+      <div className={styles.hy}>Đã hủy</div>
+      <div className={styles.hy1}>Đã hủy</div>
+      <div className={styles.pm}>10/29/2023 1:04:25PM</div>
+      <div className={styles.pm1}>10/19/2023 2:05:30PM</div>
+      <div className={styles.pm2}>9/20/2023 3:07:20PM</div>
+      <div className={styles.pm3}>8/23/2023 4:09:22PM</div>
+      <div className={styles.pm4}>7/27/2023 5:06:21PM</div>
+      <div className={styles.ph1}>Phú</div>
+      <div className={styles.khach1}>Khach</div>
+      <div className={styles.khach2}>Khach</div>
+      <div className={styles.khach3}>Khach</div>
+      <div className={styles.khach4}>khach</div>
+      <div className={styles.khach5}>khach</div>
+      <div className={styles.khach6}>khach</div>
+      <div className={styles.khach7}>khach</div>
+      <div className={styles.adminPhiuCHngChild21} />
+      <div className={styles.adminPhiuCHngChild22} />
+      <div className={styles.adminPhiuCHngChild23} />
+      <div className={styles.adminPhiuCHngChild24} />
+      <div className={styles.adminPhiuCHngChild25} />
+      <div className={styles.trc}>Trước</div>
+      <div className={styles.div}>1</div>
+      <div className={styles.div1}>2</div>
+      <div className={styles.div2}>3</div>
+      <div className={styles.sau}>Sau</div>
+      <div className={styles.lineBoxyRaglan}>LINE BOXY RAGLAN TEE - CREAM</div>
+      <div className={styles.fabricTrackPantsContainer}>
+        <span className={styles.fabricTrackPantsContainer1}>
+          <p className={styles.blankLine}>&nbsp;</p>
+          <p className={styles.blankLine}>FABRIC TRACK PANTS</p>
+        </span>
+      </div>
+      <img className={styles.adminPhiuCHngChild26} alt="" src="/line-19.svg" />
+      <div className={styles.adminPhiuCHngChild27} />
+    </div>
+  );
+};
+
+export default AdminPhiuCHng;
